9dbf42c3c259883f3430ad3349d00b442e29a718
ammar <bodla.pu@gmail.com>
2021-07-03 09:40:52 -0700

    master

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ header("Location:" . $loc . "/grab_qaqc/login.php");
             <a href="#" class="navbar-icon pull-right visible-xs" id="sidebar-toggle-btn"><i class="fa fa-search fa-lg white"></i></a>
 
           </div>
-          <a class="navbar-brand" href="#">AeroGrab</a>
+<!--          <a class="navbar-brand" href="#">QAQC</a>-->
         </div>
         <div class="navbar-collapse collapse">
           <form class="navbar-form navbar-right" role="search">
@@ -82,16 +82,16 @@ header("Location:" . $loc . "/grab_qaqc/login.php");
             <li class="hidden-xs" id="drp" style="display: none;"><a href="#" data-toggle="collapse" data-target=".navbar-collapse.in" onclick="removemarker()">Remove Marker</a></li>
               <li class="hidden-xs" id="ex" style="display: none;"><a href="#" data-toggle="collapse" data-target=".navbar-collapse.in" onclick="showExcelModel()">Export Excel</a></li>
 -->
-              <li class="hidden-xs"><a  href="services/logout.php">Logout</a></li>
-<!--              <li class="hidden-xs"><a href="#" data-toggle="collapse" data-target=".navbar-collapse.in" id="list-btn"><i class="fa fa-list white"></i>&nbsp;&nbsp;panel</a></li>-->
+              <li class="navbar-brand" href="#">QAQC</li>
+              <li class="hidden-xs"><a href="#" data-toggle="collapse" data-target=".navbar-collapse.in" id="list-btn"><i class="fa fa-list white"></i>&nbsp;&nbsp;</a></li>
 
-              <li class="hidden-xs"><div style="padding-top: 10px;padding-left: 50px;"><spsn style="color:white;">POI Search</spsn>&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text"  id="typeahead"  name="hec" class="typeahead"/></div></li>>
+              <li class="hidden-xs"><div style="padding-top: 10px;padding-left: 50px;"><spsn style="color:white;">POI Search</spsn>&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text"  id="typeahead"  name="hec" class="typeahead"/></div></li>
               <li class="hidden-xs"><button style="margin-top: 10px;margin-left: 20px"  class="btn-success" onclick="search()">Search</button></li>
-              <li class="hidden-xs"><div style="padding-top: 10px;padding-left: 50px;"><spsn style="color:white;">POI Id Search</spsn>&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text"  name="hec1" id="typeahead1" class="typeahead"/></div></li>>
+              <li class="hidden-xs"><div style="padding-top: 10px;padding-left: 50px;"><spsn style="color:white;">POI Id Search</spsn>&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text"  name="hec1" id="typeahead1" class="typeahead"/></div></li>
               <li class="hidden-xs"><button style="margin-top: 10px;margin-left: 20px"  class="btn-success" onclick="searchid()">Search</button></li>
-              <li class="hidden-xs"><div style="padding-top: 10px;padding-left: 50px;"><spsn style="color:white;">POI xy Search</spsn>&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text"  name="search_xy" id="search_xy" class="typeahead"/></div></li>>
+              <li class="hidden-xs"><div style="padding-top: 10px;padding-left: 50px;"><spsn style="color:white;">POI xy Search</spsn>&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text"  name="search_xy" id="search_xy" class="typeahead"/></div></li>
               <li class="hidden-xs"><button style="margin-top: 10px;margin-left: 20px"  class="btn-success" onclick="search_xy()">Search</button></li>
-
+              <li class="hidden-xs"><a  href="services/logout.php">Logout</a></li>
 
           </ul>
         </div><!--/.navbar-collapse -->
